bd9ed9124ed1e3d4003a8d1cb62c05fbb2ed1bf8
Richard Anderson <80434907+Richie3173@users.noreply.github.com>
2021-05-24 14:11:31 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-let num = 4
+let num = 0
 
-if (num % 15 === 0){
-   console.log('clap and stamp') 
-} else if (num % 5 === 0){
-   console.log('stamp')
-}else if (num % 3 === 0){
-   console.log('clap') 
-}else {
-   console.log(num)
+for ( num = 0; num < 101; num++) {
+
+    if (num % 15 === 0){
+        console.log('clap and stamp') 
+     } else if (num % 5 === 0){
+        console.log('stamp')
+     }else if (num % 3 === 0){
+        console.log('clap') 
+     }else {
+        console.log(num)
+     }
+     
 }
