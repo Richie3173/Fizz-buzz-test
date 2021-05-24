6bb0af9a9bb712750d8b7964674fec547af79764
Richard Anderson <80434907+Richie3173@users.noreply.github.com>
2021-05-24 13:58:32 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let num = 4
+
+if (num % 15 === 0){
+   console.log('clap and stamp') 
+} else if (num % 5 === 0){
+   console.log('stamp')
+}else if (num % 3 === 0){
+   console.log('clap') 
+}else {
+   console.log(num)
+}
